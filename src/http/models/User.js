8de044fce3e2e3c2a11fc3881c0deeb4d5cbac48
Dambi Stuart <stuartdambi@gmail.com>
2021-08-31 16:43:42 +0300

    [add] create user schema

---
+++ src/http/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  _id: mongoose.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
