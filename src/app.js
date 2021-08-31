63ed4a86f51d94c1db9edb07e934bc14b22ed72b
Dambi Stuart <stuartdambi@gmail.com>
2021-08-31 16:43:53 +0300

    [add] create database connection util

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const connectToDB = require('./utils/database');
+const userRoute = require('./http/routes/userRoute');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth/v1', userRoute);
+
+connectToDB(process.env.DATABASE_URL);
 
 const PORT = 5000 || process.env.PORT;
 
